8897239dccbb83bdc9b6741c765d209a25b87ca5
Stephanie Dawsonn-Andoh <stephandoh@gmail.com>
2025-12-04 08:30:30 +0000

    1.6 unicafe step 1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,52 +1,49 @@
-const Header = ({course}) => {
-  return <h1>{course.name}</h1>
-}
-
-const Part = ({name, exercises}) => {
-  return <p>{name} {exercises}</p>
-}
-
-const Content = ({course}) => {
-  return (
-    <div>
-        <Part name = {course.parts[0].name} exercises = {course.parts[0].exercises} />
-        <Part name = {course.parts[1].name} exercises = {course.parts[1].exercises} />
-        <Part name = {course.parts[2].name} exercises = {course.parts[2].exercises} />
-    </div>
-  )
-}  
-
-const Total = ({course}) => {
-  return (
-    <p>Number of exercises{" "} {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} </p>
-  )
-}
-
+import { useState } from "react";
 
 const App = () => {
-  const course = { name:'Half Stack application development',
-  parts : [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-  }
-  return (
-    <div>
-      <Header course = {course} />
-      <Content course = {course} />
-      <Total course = {course} />
-    </div>
-  )
-}
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-export default App
+  // Button component
+  const Button =({ onClick, text }) => (
+    <button onClick = {onClick}>{text}</button>
+  );
+
+// Log each render
+  console.log("current feedback count shows we have: good", good, "neutal:", neutral, "bad:" , bad);
+
+  const handleGoodClick = () => {
+    console.log("good button clicked. Value before click:", good);
+    setGood(good + 1);
+  };
+
+  const handleNeutralClick = () => {
+    console.log("neutral button clicked. Value before click:", neutral);
+    setNeutral(neutral + 1);
+  };
+
+  const handleBadClick = () => {
+    console.log("bad button clicked. Value before click:", bad);
+    setBad(bad + 1);
+  };
+
+  return (
+      <div>
+
+        <h1>give feedback</h1>
+
+        <Button onClick = {handleGoodClick} text = "good "/>
+        <Button onClick = {handleNeutralClick} text = "neutral "/>
+        <Button onClick = {handleBadClick} text = "bad "/>
+
+        <h1>Statistics</h1>
+
+        <p>good {good} </p>
+        <p>neutral {neutral} </p>
+        <p>bad {bad} </p>
+
+      </div>
+  );
+};
+export default App;
