102b3de73051b996764396e4f4918c5d1e82d99b
Stephanie Dawsonn-Andoh <stephandoh@gmail.com>
2025-12-04 10:14:23 +0000

    1.10 unicafe step 5

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,5 +1,11 @@
 import { useState } from "react";
 
+const StatisticLine = ({ text, value }) => {
+  return (
+    <p>{text}: {value}</p>
+  )
+}
+
 const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad;
   if (total === 0) {
@@ -10,12 +16,12 @@ const Statistics = ({ good, neutral, bad }) => {
 
   return (
     <div>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>Total: {total}</p>
-      <p>Average: {average}</p>
-      <p>Positive: {positive}%</p>
+      <StatisticLine text = "Good" value = {good} />
+      <StatisticLine text = "Neutral" value = {neutral} />
+      <StatisticLine text = "Bad" value = {bad} />
+      <StatisticLine text = "Total" value = {total} />
+      <StatisticLine text = "Average" value = {average} />
+      <StatisticLine text = "Positive" value = {positive + "%"} />
     </div>
   );
 };
