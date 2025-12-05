c45bebd637bc4f1784711c44871a92036a60750b
Stephanie Dawsonn-Andoh <stephandoh@gmail.com>
2025-12-05 07:47:39 +0000

    2.1 Course information step 6

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,67 +1,37 @@
-import { useState } from 'react'
-
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
-
 const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
-   
-  const [selected, setSelected] = useState(0)
-  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
-
-  const handleNextAnecdote = () => {
-    const randomIndex = Math.floor(Math.random() * anecdotes.length)
-    setSelected(randomIndex)
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
   }
+  // Define a component responsible for formatting a single course called Course
 
-  const handleVote = () => {
-    const votesCopy = [...votes]
-    votesCopy[selected] += 1
-    setVotes(votesCopy)
+  const Course = ({ course }) => {
+    return (
+      <div>
+        <h1>{course.name}</h1>
+        <ul>{course.parts.map(part => (<li key={part.id}>{part.name} {part.exercises}</li>))}
+        </ul>
+      </div>
+    )
   }
-
-  // what is anectode with max votes
-  const maxVotes = Math.max(...votes)
-
-  const topAnecdoteIndexes = votes
-    .map((vote, index) => (vote === maxVotes ? index : null))
-    .filter(index => index !== null)
-
-  return (
-    <div>
-      <h1>Anecdote of the day</h1>
-
-      <p>{anecdotes[selected]}</p>
-      <p>has {votes[selected]} votes</p>
-
-      <Button handleClick={handleVote} text="vote" />
-      <Button handleClick={handleNextAnecdote} text="next anecdote" />  
-
-      <h1>Anecdote(s) with most votes</h1>
-
-      {maxVotes === 0 ? (
-        <p>No votes yet</p>
-      ) : (
-        topAnecdoteIndexes.map(index => (
-          <p key={index}>
-            {anecdotes[index]} — has {maxVotes} votes
-          </p>
-        ))
-      )}
-    </div>
-  )
+  return <Course course={course} />
 }
 
 export default App
