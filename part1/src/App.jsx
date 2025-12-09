e38c0736826373ad31ca9a439862aed64682991b
Stephanie Dawsonn-Andoh <stephandoh@gmail.com>
2025-12-09 12:45:17 +0000

    2.9 The Phonebook Step 4

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
+  const [filter, setFilter] = useState('')
 
   const handleNameChange = (event) => setNewName(event.target.value)
   const handlePhoneChange = (event) => setNewPhone(event.target.value)
+  const handleFilterChange = (event) => setFilter(event.target.value)
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -18,17 +23,24 @@ const App = () => {
       return
     }
 
-    const personObject = {name: newName, number: newPhone}
+    const personObject = {name: newName, number: newPhone, id: persons.length + 1}
 
     setPersons(persons.concat(personObject))
     setNewName('')
     setNewPhone('')
   }
 
+  const checkFilter = persons.filter(person => 
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  )
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <div>
+        <h2>Phonebook</h2>  
+        filter shown with <input value={filter} onChange={handleFilterChange} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -42,8 +54,8 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
        <ul>
-        {persons.map((person, index) => (
-          <li key={index}>{person.name} - {person.number}</li>
+        {checkFilter.map((person) => (
+          <li key={person.id}>{person.name} - {person.number}</li>
         ))} 
       </ul>
     </div>
